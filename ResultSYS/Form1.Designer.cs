06dddca20e4df78f0872d6f96d4cc05d3e4f991e
charliekato <ykato449876@gmail.com>
2023-01-15 19:13:10 +0900

    initial commit

---
+++ ResultSYS/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ResultSys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnShowLaneOrder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBxFolder = new System.Windows.Forms.TextBox();
+            this.btnFolderSelect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbxDbContents = new System.Windows.Forms.ListBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnShowLaneOrder
+            // 
+            this.btnShowLaneOrder.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnShowLaneOrder.Location = new System.Drawing.Point(1149, 215);
+            this.btnShowLaneOrder.Name = "btnShowLaneOrder";
+            this.btnShowLaneOrder.Size = new System.Drawing.Size(254, 85);
+            this.btnShowLaneOrder.TabIndex = 0;
+            this.btnShowLaneOrder.Text = "レーン順表示";
+            this.btnShowLaneOrder.UseVisualStyleBackColor = true;
+            this.btnShowLaneOrder.Click += new System.EventHandler(this.btnShowLaneOrder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(537, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 60);
+            this.label1.TabIndex = 1;
+            // 
+            // txtBxFolder
+            // 
+            this.txtBxFolder.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtBxFolder.Location = new System.Drawing.Point(367, 88);
+            this.txtBxFolder.Name = "txtBxFolder";
+            this.txtBxFolder.Size = new System.Drawing.Size(1165, 43);
+            this.txtBxFolder.TabIndex = 2;
+            // 
+            // btnFolderSelect
+            // 
+            this.btnFolderSelect.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnFolderSelect.Location = new System.Drawing.Point(442, 215);
+            this.btnFolderSelect.Name = "btnFolderSelect";
+            this.btnFolderSelect.Size = new System.Drawing.Size(283, 85);
+            this.btnFolderSelect.TabIndex = 3;
+            this.btnFolderSelect.Text = "パス設定";
+            this.btnFolderSelect.UseVisualStyleBackColor = true;
+            this.btnFolderSelect.Click += new System.EventHandler(this.btnFolderSelect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(29, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(282, 42);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "データベースパス";
+            // 
+            // lbxDbContents
+            // 
+            this.lbxDbContents.Font = new System.Drawing.Font("ＭＳ ゴシック", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbxDbContents.FormattingEnabled = true;
+            this.lbxDbContents.ItemHeight = 30;
+            this.lbxDbContents.Location = new System.Drawing.Point(12, 334);
+            this.lbxDbContents.Name = "lbxDbContents";
+            this.lbxDbContents.Size = new System.Drawing.Size(2100, 844);
+            this.lbxDbContents.TabIndex = 6;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(1935, 49);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(121, 61);
+            this.btnQuit.TabIndex = 8;
+            this.btnQuit.Text = "終了";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2116, 1195);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.lbxDbContents);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFolderSelect);
+            this.Controls.Add(this.txtBxFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShowLaneOrder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowLaneOrder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBxFolder;
+        private System.Windows.Forms.Button btnFolderSelect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbxDbContents;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.ToolTip toolTip2;
+    }
+}
+
---
+++ ResultSYS/Form1.cs
@@ -0,0 +1,308 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+
+namespace ResultSys
+{
+    public class setup_file_io
+    {
+
+        
+        private const string setupFileName = "swmsys.setup.sjis.txt";
+
+        public static void write_setup_file(string dbPath )
+        {
+            string[] lineBuf = new string[10];
+            string line;
+            int lineNum = 0;
+            int lastLine=0;
+            try
+            {
+
+                using (StreamReader reader = new StreamReader(setupFileName, System.Text.Encoding.GetEncoding("sjis")))
+                {
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        lineBuf[lineNum] = line;
+                        string[] words = line.Split('>');
+                        if (words[0]=="DBPATH")
+                        {
+                            lineBuf[lineNum] = "DBPATH>" + dbPath;
+                        }
+                        lineNum++;
+                    }
+                }
+                lastLine = lineNum;
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(setupFileName, false, System.Text.Encoding.GetEncoding("sjis")))
+                {
+                    for (lineNum=0;lineNum<lastLine;lineNum++)
+                    {
+                        writer.WriteLine(lineBuf[lineNum]);
+                    }
+                }
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+
+
+        public static void read_setup_file(ref string resultDbPath, ref string dbPath,ref int interval2NextRace, ref int lapAliveTime)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(setupFileName, System.Text.Encoding.GetEncoding("sjis")))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line == "") continue;
+                        if (line.Substring(0, 1) == "#") continue;
+                        string[] words = line.Split('>');
+                        if (words[0]=="DBPATH")
+                        {
+                            dbPath = words[1];
+                        }
+                        if (words[0]=="INTERVAL2NEXTRACE")
+                        {
+                            interval2NextRace = Int32.Parse(words[1]);
+                        }
+                        if (words[0]=="LAPALIVETIME")
+                        {
+                            lapAliveTime = Int32.Parse(words[1]);
+                        }
+                    }
+                }
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+
+    }
+    public partial class Form1 : Form
+    {
+        string folderName;
+        public static int interval2NextRace;
+        public static int lapAliveTime;
+        public const int NUMSTYLE = 7;
+        public const int NUMDISTANCE = 7;
+
+        public const int TIME4DNS = 999999;
+        public const int TIME4DQ = 999998;
+        public const int DNS = 1;
+        public const int DQ = 2;
+
+
+        static Encoding shiftjisEnc = Encoding.GetEncoding("Shift-JIS");
+        public bool StopAutoRun;
+        // related 大会設定
+        public string EventName;
+        public string EventDate;
+        public string EventVenue;
+
+        
+        static public string[] Team = new string[100];
+        static public int LastPrgNo;
+        static public int FirstPrgNo;
+        static public int MaxTeamNum=0;
+        // for チームマスター
+        static public string[] TeamName4Relay;
+        static public Form2 form2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            init_size(930,780);
+            set_size_of_list_box(900, 520);
+            setup_file_io.read_setup_file(ref ExcelConnection.mdbFile,ref folderName,ref interval2NextRace,ref lapAliveTime);
+            //MessageBox.Show("db path  " + folderName + " " + interval2NextRace + " " + lapAliveTime);
+            ExcelConnection.delete();
+            if (folderName != "")
+            {
+                
+                txtBxFolder.Text = folderName;
+                call_showEventList();
+            }   
+        }
+        private void set_size_of_list_box(int width, int height)
+        {
+            lbxDbContents.Width = width;
+            lbxDbContents.Height = height;
+        }
+        private void init_size(int width, int height) {
+            this.Width = width;
+            this.Height = height;
+        }
+
+
+        static string get_file(string initFile)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.FileName = initFile;
+            ofd.InitialDirectory = @"C:\Users\ykato";
+            ofd.Filter = "エクセルファイル(*.xlsx)|*.xlsx";
+            ofd.FilterIndex = 2;
+            ofd.Title = "タイムデータが入っているエクセルファイルを選択してください";
+            ofd.RestoreDirectory = true;
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                Console.WriteLine("> {0} <", ofd.FileName);
+                return (ofd.FileName);
+            }
+            return (initFile);
+        }
+
+        private void btnShowLaneOrder_Click(object sender, EventArgs e)
+        {
+
+            string selectedString;
+            string[] sep = { " " };
+
+            selectedString = lbxDbContents.SelectedItem.ToString();
+            //------------------------
+            string[] eventInfo = selectedString.Split(sep,StringSplitOptions.RemoveEmptyEntries);
+            string fullpathDBName = folderName + "\\" + eventInfo[0];
+            
+            
+             
+
+            form2 = new Form2(fullpathDBName);
+            form2.Show();
+        }
+        private void call_showEventList()
+        {
+            DialogResult dr=MessageBox.Show("大会名を読み込みます","OK?",MessageBoxButtons.YesNo);
+            if (dr==System.Windows.Forms.DialogResult.Yes)
+            {
+                this.Cursor = Cursors.WaitCursor;
+                folderName = txtBxFolder.Text;
+                showEventList(folderName);
+                this.Cursor = Cursors.Default;
+            }
+            
+        }
+        private void btnFolderSelect_Click(object sender, EventArgs e)
+        {
+            
+            FolderBrowserDialog dbPickDialog = new FolderBrowserDialog();
+            dbPickDialog.Description = "データベースパスの選択";
+            dbPickDialog.RootFolder = Environment.SpecialFolder.UserProfile;
+            dbPickDialog.SelectedPath = folderName;
+            DialogResult result = dbPickDialog.ShowDialog();
+            if (result==DialogResult.OK)
+            {
+                folderName = dbPickDialog.SelectedPath;
+                txtBxFolder.Text = folderName;
+                setup_file_io.write_setup_file(folderName);
+                call_showEventList();
+                
+            }
+        }
+        
+
+        public static void Read_event_db(string dbFileName,ref string eventName, ref string eventDate, ref string eventVenue)
+        {
+            String connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+              "Data Source="+dbFileName;
+
+            event_db evtDB = new event_db(connectionString);
+            
+            {
+                eventName = evtDB.get_eventName();
+                eventDate = evtDB.get_eventDate();
+                eventVenue = evtDB.get_eventVenue();
+            }
+                
+        }
+       
+        public void showEventList(string dbPath)
+        {
+            string[] eventNameA = new string[100];
+            string[] eventVenueA = new string[100];
+            string[] eventDateA = new string[100];
+            string[] fileName = new string[100];
+            int counter = 0;
+            int numRecords;
+
+            int myLength;
+            int[] myByteLen = new int[100];
+            string eventName = "";
+            string myDate = "";
+            string myVenue = "";
+
+            lbxDbContents.Items.Clear();
+
+
+            try
+            {
+                var txtFiles = Directory.EnumerateFiles(dbPath, "*.mdb");
+                int maxEventNameLength = 0;
+                foreach (string currentFile in txtFiles)
+                {
+                    fileName[counter] = currentFile.Substring(currentFile.Length - 10);
+                    try
+                    {
+                        Read_event_db(currentFile, ref eventName, ref myDate, ref myVenue);
+                        if (eventName != null)
+                        {
+                            myLength = eventName.Length;
+                            myByteLen[counter] = shiftjisEnc.GetByteCount(eventName);
+                            if (maxEventNameLength < myByteLen[counter]) maxEventNameLength = myByteLen[counter];
+                            eventNameA[counter] = eventName;
+                            eventDateA[counter] = myDate;
+                            eventVenueA[counter] = myVenue;
+                            counter++;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                                              MessageBox.Show(e.Message);
+                    }
+                }
+                numRecords = counter - 1;
+                lbxDbContents.Items.Add("File名      大会名" + new string(' ', maxEventNameLength + 2) +
+                    "期間" + new string(' ', 14) + "場所");
+                for (counter = 0; counter <= numRecords; counter++)
+                {
+                    lbxDbContents.Items.Add(fileName[counter] + "  " + eventNameA[counter] +
+                        new string(' ', (maxEventNameLength + 2 - myByteLen[counter]))
+                         + eventDateA[counter] + "  " + eventVenueA[counter]);
+                }
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
+ 
+
+
+        
